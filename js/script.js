dcb2471b2b534b52c492e15ee6369476776c31e9
Emiko2903 <seaman2903@mail.ru>
2021-11-03 21:12:37 +0400

    fff

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// task - 1
+
+// let task1 = 2
+
+// if (task1%7 == 0 && task1%3 == 0) {
+//     console.log ("Всё тип топ")
+// } else {
+//     console.log("У нас проблемы Хьюстон")
+// }
+
+// task-2 
+
+
+// let task2 = -6
+
+// if (task2<=45 && task2>=30) {
+//     console.log("Капец Что то жарковато")
+// } else if (task2<=30 && task2>=20) {
+//     console.log ("Все нормуль") 
+// } else if (task2<=20 && task2>=-10) {
+//     console.log ("Писец как холодно")
+// }
+
+// task-3
+
+
+// let i = 0
+
+// for (let x = 0; x <= 10; x++) {
+//     i = i + x
+//     console.log (i)
+// }
+
+
+// task-4
+
+
+// let x = prompt("Введите чило для проверки")
+
+// if (x % 2 > 0) {
+//     console.log("Это нечетное число")
+// } else {
+//     console.log("Это четное число")
+// }
